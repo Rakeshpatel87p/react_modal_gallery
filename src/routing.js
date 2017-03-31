a9dd462bafccce530159e2465236d2e1584f924f
Rakesh Patel <emailme@rakeshrpatel.com>
2017-03-31 13:59:27 -0400

    cleaned up code, almost ready to ship

--- src/routing.js
+++ src/routing.js
@@ -1,36 +1,8 @@
-import React from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom'
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import Shows from './shows';
 
-
-// This example shows how to render two different screens
-// (or the same screen in a different context) at the same url,
-// depending on you got there.
-//
-// Click the colors and see them full screen, then "visit the
-// gallery" and click on the colors. Note the URL and the component
-// are the same as before but now we see them inside a modal
-// on top of the old screen.
-
 class ModalSwitch extends React.Component {
-
-  // We can pass a location to <Switch/> that will tell it to
-  // ignore the router's current location and use the location
-  // prop instead.
-  //
-  // We can also use "location state" to tell the app the user
-  // wants to go to `/images/2` in a modal, rather than as the
-  // main page, keeping the gallery visible behind it.
-  //
-  // Normally, `/images/2` wouldn't match the gallery at `/`.
-  // So, to get both screens to render, we can save the old
-  // location and pass it to Switch, so it will think the location
-  // is still `/` even though its `/images/2`.
   previousLocation = this.props.location
 
   componentWillUpdate(nextProps) {
@@ -46,7 +18,6 @@ class ModalSwitch extends React.Component {
 
   render() {
     const { location } = this.props
-    console.log('this be modal! ' + isModal)
     const isModal = !!(
       location.state &&
       location.state.modal &&
@@ -56,46 +27,13 @@ class ModalSwitch extends React.Component {
       <div>
         <Switch location={isModal ? this.previousLocation : location}>
           <Route path='/shows' component={Gallery}/>
-          <Route path='/shows/:id' component={ImageView}/>
+          <Route path='/shows/:id' component={Modal}/>
         </Switch>
         {isModal ? <Route path='/shows/:id' component={Modal} /> : null}
       </div>
     )
   }
 }
-
-const IMAGES = [
-  { id: 0, title: 'Dark Orchid', color: 'DarkOrchid' },
-  { id: 1, title: 'Lime Green', color: 'LimeGreen' },
-  { id: 2, title: 'Tomato', color: 'Tomato' },
-  { id: 3, title: 'Seven Ate Nine', color: '#789' },
-  { id: 4, title: 'Crimson', color: 'Crimson' }
-]
-
-const Thumbnail = ({ color }) =>
-  <div style={{
-    width: 50,
-    height: 50,
-    background: color
-  }}/>
-
-const Image = ({ color }) =>
-  <div style={{
-    width: '100%',
-    height: 400,
-    background: color
-  }}></div>
-
-const Home = () => (
-  <div>
-    <Link to='/gallery'>Visit the Gallery</Link>
-    <h2>Featured Images</h2>
-    <ul>
-      <li><Link to='/img/2'>Tomato</Link></li>
-      <li><Link to='/img/4'>Crimson</Link></li>
-    </ul>
-  </div>
-)
 
 const Gallery = () => (
   <div>
@@ -104,33 +42,17 @@ const Gallery = () => (
         key={i.id}
         to={{
           pathname: `/shows/${i.id}`,
-          // this is the trick!
           state: { modal: true }
         }}
       >
-        <p>{i.title}</p>
         <img alt={i.title} src={i.product_image_url} />
       </Link>
     ))}
   </div>
 )
 
-const ImageView = ({ match }) => {
-  const image = IMAGES[parseInt(match.params.id, 10)]
-  if (!image) {
-    return <div>Image not found</div>
-  }
-
-  return (
-    <div>
-      <h1>{image.title}</h1>
-      <Image color={image.color} />
-    </div>
-  )
-}
-
 const Modal = ({ match, history }) => {
-  const show = Shows[parseInt(match.params.id, 10)]
+  const show = Shows[parseInt(match.params.id, 10) -1]
   if (!show) {
     return null
   }
